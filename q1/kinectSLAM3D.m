679249759834ab71d52bedd35b5bb2885ba52e00
Connor Smith <con.smith13@gmail.com>
2014-12-13 11:18:55 -0500

    initial belief and add storage arrays

--- q1/kinectSLAM3D.m
+++ q1/kinectSLAM3D.m
@@ -42,9 +42,26 @@ isNewFeature = ones(featureNum,1);
 
 %% Simulation Initializations
 initialState = [0 10 0]';
-state = zeros(vehicleStateNum,length(T)); % Vehicle states 
-state(:,1) = initialState;
+trueState = zeros(vehicleStateNum,length(T)); % State history 
+trueState(:,1) = initialState;
 fullStateNum = vehicleStateNum+measPerFeature*featureNum; % # of SLAM states
+meas = zeros(measPerFeature*featureNum,length(T)); % Measurement history
+
+% Prior over vehicle state
+vehiclePrior = [0 0 0]';
+vehiclePriorCov = 0.00000000001*eye(3);
+
+% Prior over feature map
+mapPrior = zeros(measPerFeature*featureNum,1);
+mapPriorCov = 100*eye(measPerFeature*featureNum);
+
+% Initial Full State Belief and Covariance
+mu = [vehiclePrior; mapPrior];
+S = [vehiclePriorCov zeros(vehicleStateNum,measPerFeature*featureNum);
+    zeros(measPerFeature*featureNum,vehicleStateNum) mapPriorCov];
+
+mu_S = zeros(fullStateNum,length(T)); % Belief history
+mu_S(:,1) = mu; % Initial belief
 
 %% Simulation Loop
 for i=1:length(T)-1
@@ -53,20 +70,20 @@ for i=1:length(T)-1
     motionDisturbance = mvnrnd([0 0 0], motionDistCov)';
 
     % Position update
-    state(1,i+1) = state(1,i) + dt*speed(i)*cos(state(3,i));
-    state(2,i+1) = state(2,i) + dt*speed(i)*sin(state(3,i));
-    state(3,i+1) = state(3,i) - dt*speed(i)*(tan(steerAngle(i))/wheelbase);
+    trueState(1,i+1) = trueState(1,i) + dt*speed(i)*cos(trueState(3,i));
+    trueState(2,i+1) = trueState(2,i) + dt*speed(i)*sin(trueState(3,i));
+    trueState(3,i+1) = trueState(3,i) - dt*speed(i)*(tan(steerAngle(i))/wheelbase);
 
-    state(:,i+1) = state(:,i+1) + motionDisturbance;
+    trueState(:,i+1) = trueState(:,i+1) + motionDisturbance;
 end
 
 %% Plotting
 % Trajectory figure
 figure(1);clf; hold on;
 % initial location marker
-plot(state(1,1),state(2,1),'gd','MarkerSize',10,'MarkerFaceColor','g');
+plot(trueState(1,1),trueState(2,1),'gd','MarkerSize',10,'MarkerFaceColor','g');
 % plot the entire path
-plot(state(1,:),state(2,:),'r');
+plot(trueState(1,:),trueState(2,:),'r');
 plot(map(1,:),map(2,:),'oc');
 xlabel('X [m]','FontSize',14)
 ylabel('Y [m]','FontSize',14)
